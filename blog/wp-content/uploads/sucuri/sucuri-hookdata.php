02902cc41d27592637ab5e602f422068b7ac46dd
Sorav Garg <sorav.mobiwebtech123@gmail.com>
2019-08-14 10:15:31 +0530

    remove blog folder

--- blog/wp-content/uploads/sucuri/sucuri-hookdata.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=hookdata;
-// created_on=1556807882;
-// updated_on=1559820015;
-exit(0);
-?>
--- blog/wp-content/uploads/sucuri/sucuri-ignorescanning.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=ignorescanning;
-// created_on=1556612984;
-// updated_on=1556612984;
-exit(0);
-?>
