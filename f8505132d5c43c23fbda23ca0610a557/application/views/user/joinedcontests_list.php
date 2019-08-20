18f161589ec5d1d3147bc2291b0448a6037e3c3d
kamlesh24 <kamlesh@mobiwebtech.in>
2019-08-20 20:13:06 +0530

    joind contest working

--- f8505132d5c43c23fbda23ca0610a557/application/views/user/joinedcontests_list.php
+++ f8505132d5c43c23fbda23ca0610a557/application/views/user/joinedcontests_list.php
@@ -16,10 +16,13 @@
 			<span ng-if="data.dataList.length" class="h5">Total records: {{data.totalRecords}}</span>
 		</span>
 		<div class="float-right">
-			<form id="filterForm" role="form" autocomplete="off" ng-submit="applyFilter()" class="ng-pristine ng-valid">
-				<input type="text" class="form-control ml-1" name="Keyword" placeholder="Search">
-			</form>
-		</div>
+	        <a href="user"><button class="btn btn-success btn-sm ml-1">Back</button></a>
+	    </div>
+		
+		<div class="float-right">
+			<button class="btn btn-default btn-secondary btn-sm ng-scope" data-toggle="modal" data-target="#filter_model"><img src="asset/img/filter.svg"></button>&nbsp;
+		</div>	
+		
 	</div>
 	<!-- Top container/ -->
 
@@ -55,7 +58,9 @@
 				<tr scope="row" ng-repeat="(key, row) in data.dataList" id="sectionsid_{{row.MenuOrder}}.{{row.CategoryID}}">
 				
 					<td>
-						<strong>{{row.ContestName}}</strong>
+						<strong>
+							<a href="javascript:void(0)" ng-click="loadContestJoinedUser(key,row.ContestGUID)">{{row.ContestName}}</a>
+						</strong>
 					</td>
 					<td>
 						<p>{{!row.TeamNameLocal ? '-' : row.TeamNameLocal }}</p>
@@ -73,7 +78,7 @@
 						<p>{{row.Privacy}}</p>
 					</td>
 					<td>
-						<p>{{row.EntryFee}}</p>
+						<p>{{DEFAULT_CURRENCY}}{{row.EntryFee}}</p>
 					</td>
 					<td>
 						<p>{{row.EntryType}}</p>
@@ -82,15 +87,16 @@
 						<p>{{row.NoOfWinners}}</p>
 					</td>
 					<td>
-						<p>{{row.WinningAmount}}</p>
+						<p>{{DEFAULT_CURRENCY}}{{row.WinningAmount}}</p>
 					</td>
-					<td class="text-center"><span ng-class="{Inactive:'text-danger', Active:'text-success'}[row.StatusID]">{{row.Status}}</span></td> 
+					<td class="text-center"><span ng-class="{Pending:'text-secondary', Completed:'text-success',Cancelled:'text-danger',Running:'text-primary'}[row.Status]">{{row.Status}}</span></td> 
 					<td class="text-center">
 						<div class="dropdown">
 							<button class="btn btn-secondary  btn-sm action" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&#8230;</button>
 							<div class="dropdown-menu dropdown-menu-left">
-								<a class="dropdown-item" href="" ng-click="loadWinnersForm(key, row.ContestGUID)">Custom Winning</a>
-								<a class="dropdown-item" href="" ng-click="loadParticipatedTeamForm(key, row.ContestGUID)">Participated Teams</a>
+								<a class="dropdown-item" href=""
+                                    ng-click="loadContestJoinedUser(key,row.ContestGUID)">Details</a>
+                                <a class="dropdown-item" href="" ng-click="loadTeams(row)">View Team Players</a>
 							</div>
 						</div>
 					</td> 
@@ -109,49 +115,151 @@
 	<!-- Data table/ -->
 
 
-
-
 	<!-- Filter Modal -->
-	<div class="modal fade" id="filter_model"  ng-init="getFilterData()">
-		<div class="modal-dialog modal-md" role="document">
+	<div class="modal fade" id="filter_model" ng-init="getFilterData()">
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title h5">Filters</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                </div>
+
+                <!-- Filter form -->
+                <form id="filterForm" role="form" autocomplete="off" class="ng-pristine ng-valid">
+                    <div class="modal-body">
+                        <div class="form-area">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ParentCategory">Series</label>
+                                        <select id="Series" name="SeriesGUID" ng-model="SeriesGUID"
+                                            class="form-control chosen-select" ng-change="getMatches(SeriesGUID)">
+                                            <option value="">Please Select</option>
+                                            <option ng-repeat="Series in filterData.SeiresData"
+                                                value="{{Series.SeriesGUID}}">{{Series.SeriesName}}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ParentCategory">Match</label>
+                                        <select id="MatchGUID" name="MatchGUID" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option class="filter-matches" ng-repeat="match in MatchData"
+                                                value="{{match.MatchGUID}}">{{match.TeamNameLocal}} Vs
+                                                {{match.TeamNameVisitor}} ON {{match.MatchStartDateTime}}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ContestFormat">Contest Format</label>
+                                        <select id="ContestFormat" name="ContestFormat"
+                                            class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Head to Head">Head to Head</option>
+                                            <option value="League">League</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ContestType">Contest Type</label>
+                                        <select id="ContestType" name="ContestType" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Normal">Normal</option>
+                                            <option value="Reverse">Reverse</option>
+                                            <option value="InPlay">InPlay</option>
+                                            <option value="Hot">Hot</option>
+                                            <option value="Champion">Champion</option>
+                                            <option value="Practice">Practice</option>
+                                            <option value="More">More</option>
+                                            <option value="Mega">Mega</option>
+                                            <option value="Winner Takes All">Winner Takes All</option>
+                                            <option value="Only For Beginners">Only For Beginners</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="Privacy">Privacy</label>
+                                        <select id="Privacy" name="Privacy" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Yes">Yes</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="IsPaid">Is Paid</label>
+                                        <select id="IsPaid" name="IsPaid" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Yes">Yes</option>
+                                            <option value="No">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="EntryType">Entry Type</label>
+                                        <select id="EntryType" name="EntryType" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Single">Single</option>
+                                            <option value="Multiple">Multiple</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="Status">Contest Status</label>
+                                        <select id="Status" name="Status" class="form-control chosen-select">
+                                            <option value="">Please Select</option>
+                                            <option value="Pending">Pending</option>
+                                            <option value="Running">Running</option>
+                                            <option value="Cancelled">Cancelled</option>
+                                            <option value="Completed">Completed</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ParentCategory">Search</label>
+                                        <input type="text" class="form-control" name="Keyword" placeholder="Search">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> <!-- form-area /-->
+                    </div> <!-- modal-body /-->
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary btn-sm"
+                            onclick="$('.filter-matches').remove();$('#filterForm').trigger('reset'); $('.chosen-select').trigger('chosen:updated');">Reset</button>
+                        <button type="submit" class="btn btn-success btn-sm" data-dismiss="modal"
+                            ng-disabled="editDataLoading" ng-click="applyFilter()">Apply</button>
+                    </div>
+
+                </form>
+                <!-- Filter form/ -->
+            </div>
+        </div>
+    </div>
+
+    <!-- contest joined user Modal -->
+	<div class="modal fade" id="contestJoinedUsers_model">
+		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h3 class="modal-title h5">Filters</h3>     	
+					<h3 class="modal-title h5"><?php echo $this->ModuleData['ModuleName'];?></h3>     	
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				</div>
-
-				<!-- Filter form -->
-				<form id="filterForm1" role="form" autocomplete="off" class="ng-pristine ng-valid">
-					<div class="modal-body">
-						<div class="form-area">
-
-							<div class="row">
-								<div class="col-md-8">
-									<div class="form-group">
-										<label class="filter-col" for="StatusID">Status</label>
-										<select id="StatusID" name="StatusID" class="form-control chosen-select">
-											<option value="">Please Select</option>
-											<option value="2">Active</option>
-											<option value="6">Inactive</option>
-										</select>   
-									</div>
-								</div>
-							</div>
-
-						</div> <!-- form-area /-->
-					</div> <!-- modal-body /-->
-
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary btn-sm" onclick="$('#filterForm').trigger('reset'); $('.chosen-select').trigger('chosen:updated');">Reset</button>
-						<button type="submit" class="btn btn-success btn-sm" data-dismiss="modal" ng-disabled="editDataLoading" ng-click="applyFilter()">Apply</button>
-					</div>
-
-				</form>
-				<!-- Filter form/ -->
+				<div ng-include="templateURLEdit"></div>
 			</div>
 		</div>
 	</div>
-
 
 
 	<!-- add Modal -->
--- f8505132d5c43c23fbda23ca0610a557/asset/js/joinedcontests.js
+++ f8505132d5c43c23fbda23ca0610a557/asset/js/joinedcontests.js
@@ -1,7 +1,25 @@
 app.controller('PageController', function ($scope, $http,$timeout){
+    $scope.DEFAULT_CURRENCY = DEFAULT_CURRENCY;
+    $scope.data.DEFAULT_CURRENCY = DEFAULT_CURRENCY;
     $scope.data.pageSize = 15;
     /*----------------*/
-     /*list*/
+    /*list*/
+    $scope.getFilterData = function ()
+    {
+        var data = 'SessionKey='+SessionKey+'&Params=SeriesName,SeriesGUID&StatusID=2&'+$('#filterPanel form').serialize();
+
+        $http.post(API_URL+'admin/matches/getFilterData', data, contentType).then(function(response) {
+            var response = response.data;
+            if(response.ResponseCode==200 && response.Data){ 
+            /* success case */
+                $scope.filterData =  response.Data;
+                $timeout(function(){
+                    $("select.chosen-select").chosen({ width: '100%',"disable_search_threshold": 8}).trigger("chosen:updated");
+                }, 300);          
+            }
+        });
+    }
+
     $scope.applyFilter = function() {
         $scope.data = angular.copy($scope.orig); /*copy and reset from original scope*/
         $scope.getList();
@@ -25,7 +43,7 @@ app.controller('PageController', function ($scope, $http,$timeout){
         $scope.getUserInfo();
         if ($scope.data.listLoading || $scope.data.noRecords) return;
         $scope.data.listLoading = true;
-        var data = 'SessionKey='+SessionKey+'&UserGUID='+getQueryStringValue('UserGUID')+'&Params=Privacy,IsPaid,WinningAmount,ContestSize,EntryFee,NoOfWinners,EntryType,Status,TeamNameLocal,TeamNameVisitor&PageNo=' + $scope.data.pageNo + '&PageSize=' + $scope.data.pageSize + '&OrderBy=' + $scope.data.OrderBy + '&Sequence=' + $scope.data.Sequence + '&' + $('#filterForm').serialize();
+        var data = 'SessionKey='+SessionKey+'&UserGUID='+getQueryStringValue('UserGUID')+'&Params=Privacy,IsPaid,WinningAmount,ContestSize,EntryFee,NoOfWinners,EntryType,Status,TeamNameLocal,UserTeamPlayers,TeamNameVisitor&PageNo=' + $scope.data.pageNo + '&PageSize=' + $scope.data.pageSize + '&OrderBy=' + $scope.data.OrderBy + '&Sequence=' + $scope.data.Sequence + '&' + $('#filterForm').serialize();
         $http.post(API_URL+'admin/contest/getUserJoinedContests', data, contentType).then(function(response) {
         var response = response.data;
         manageSession(response.ResponseCode);
@@ -39,6 +57,25 @@ app.controller('PageController', function ($scope, $http,$timeout){
                 $scope.data.noRecords = true;
             }
             $scope.data.listLoading = false;
+        });
+    }
+
+    $scope.loadContestJoinedUser = function (Position, ContestGUID) {
+
+        $scope.data.Position = Position;
+        $scope.templateURLEdit = PATH_TEMPLATE + 'contests/joinedContest_form.htm?' + Math.random();
+        $scope.data.pageLoading = true;
+        $http.post(API_URL + 'contest/getContests', 'SessionKey=' + SessionKey + '&ContestGUID=' + ContestGUID + '&Params=Privacy,IsPaid,WinningAmount,ContestSize,EntryFee,NoOfWinners,EntryType,TeamNameLocal,TeamNameShortLocal,TeamFlagLocal,TeamNameVisitor,TeamNameShortVisitor,TeamFlagVisitor,SeriesName,CustomizeWinning,ContestType,CashBonusContribution,UserJoinLimit,ContestFormat,IsConfirm,ShowJoinedContest,TotalJoined,AdminPercent,UnfilledWinningPercent,TotalAmountReceived,TotalWinningAmount,Status,MatchStartDateTime', contentType).then(function (response) {
+            var response = response.data;
+            manageSession(response.ResponseCode);
+            if (response.ResponseCode == 200) { /* success case */
+                $scope.data.pageLoading = false;
+                $scope.contestData = response.Data;
+                $('#contestJoinedUsers_model').modal({ show: true });
+                $timeout(function () {
+                    $(".chosen-select").chosen({ width: '100%', "disable_search_threshold": 8, "placeholder_text_multiple": "Please Select", }).trigger("chosen:updated");
+                }, 200);
+            }
         });
     }
 
@@ -93,6 +130,12 @@ app.controller('PageController', function ($scope, $http,$timeout){
             }
         });
 
+    }
+
+    $scope.loadTeams = function (row) {
+        $scope.playerData = row.UserTeamPlayers;
+        $scope.templateURLEdit = PATH_TEMPLATE + module + '/getTeams_form.htm?' + Math.random();
+        $('#viewTeams_model').modal({ show: true });
     }
 
 
