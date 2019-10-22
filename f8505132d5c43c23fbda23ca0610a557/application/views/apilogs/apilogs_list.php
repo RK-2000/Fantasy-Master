ad76325a0ab60e9debeb82cfcceb0be0b45394c8
Sorav Garg <sorav.mobiwebtech123@gmail.com>
2019-10-22 18:11:04 +0530

    work on api logs

--- f8505132d5c43c23fbda23ca0610a557/application/views/apilogs/apilogs_list.php
+++ f8505132d5c43c23fbda23ca0610a557/application/views/apilogs/apilogs_list.php
@@ -7,24 +7,23 @@
     <!-- Top container -->
     <div class="clearfix mt-2 mb-2">
         <span class="float-left records d-none d-sm-block">
-            <span ng-if="data.dataList.length" class="h5">Total records: {{data.totalRecords}}</span>
+            <p ng-if="data.dataList.length" class="h5">Total records: {{data.totalRecords}}</p>
+            <p><strong class="api_status" ng-class="{false:'text-danger', true:'text-success'}[data.IsAPILogs]">API Save Log Current Status : {{data.IsAPILogs == true ? 'On' : 'Off'}}</strong></p>
         </span>
-        <div class="float-right mr-2">		
-			<button class="btn btn-success btn-sm ml-1 float-right" ng-click="deleteAll();">Delete All</button>
-		</div>
+        <div class="float-right mr-2">
+            <button class="btn btn-success btn-sm ml-1 float-right" ng-click="deleteAll();" title="Click Here To Delete All API Logs">Delete All</button>
+        </div>
         <div class="float-right">
-			<button class="btn btn-default btn-secondary btn-sm ng-scope" data-toggle="modal" data-target="#filter_model"><img src="asset/img/filter.svg"></button>&nbsp;
-		</div>
+            <button class="btn btn-default btn-secondary btn-sm ng-scope" data-toggle="modal" data-target="#filter_model"><img src="asset/img/filter.svg"></button>&nbsp;
+        </div>
     </div>
     <!-- Top container/ -->
 
 
     <!-- Data table -->
-    <div class="table-responsive block_pad_md" infinite-scroll="getList()" infinite-scroll-disabled='data.listLoading'
-        infinite-scroll-distance="0">
+    <div class="table-responsive block_pad_md" infinite-scroll="getList()" infinite-scroll-disabled='data.listLoading' infinite-scroll-distance="0">
         <!-- loading -->
         <p ng-if="data.listLoading" class="text-center data-loader"><img src="asset/img/loader.svg"></p>
-       <p><strong>Api Log : {{data.IsAPILogs == true ? 'On' : 'Off'}}</strong></p>
         <!-- data table -->
         <table class="table table-striped table-condensed table-hover table-sortable" ng-if="data.dataList.length">
             <!-- table heading -->
@@ -39,30 +38,30 @@
             </thead>
             <!-- table body -->
             <tbody id="tabledivbody">
-				<tr scope="row" ng-repeat="(key, row) in data.dataList" id="sectionsid_{{row.MenuOrder}}.{{row.CategoryID}}">
-					<td>
-                        {{row._id.$oid}} 
+                <tr scope="row" ng-repeat="(key, row) in data.dataList" id="sectionsid_{{row.MenuOrder}}.{{row.CategoryID}}">
+                    <td>
+                        {{row._id.$oid}}
                     </td>
                     <td>
-                        {{row.DataJ.API}}  
-					</td>
-                    <td>
-                        {{row.URL}} 
+                        {{row.DataJ.API}}
                     </td>
                     <td>
-                        {{row.EntryDate}} 
-					</td>
-					<td class="text-center">
-						<div class="dropdown">
-							<button class="btn btn-secondary  btn-sm action" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&#8230;</button>
-							<div class="dropdown-menu dropdown-menu-left">
+                        {{row.URL}}
+                    </td>
+                    <td>
+                        {{row.EntryDate}}
+                    </td>
+                    <td class="text-center">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary  btn-sm action" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&#8230;</button>
+                            <div class="dropdown-menu dropdown-menu-left">
                                 <a class="dropdown-item" href="" ng-click="deleteAPILog(key, row._id.$oid)">Delete</a>
                                 <a class="dropdown-item" href="" ng-click="viewAPILog(row)">View Details</a>
-							</div>
-						</div>
-					</td>
-				</tr>
-			</tbody>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
         </table>
 
         <!-- no record -->
@@ -91,12 +90,12 @@
                         <div class="form-area">
 
                             <div class="row">
-                            <div class="col-md-6">
-								<div class="form-group">
-									<label class="filter-col" for="ParentCategory">Search</label>
-									<input type="text" class="form-control" name="Keyword" placeholder="Search">
-								</div>
-							</div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="filter-col" for="ParentCategory">Search</label>
+                                        <input type="text" class="form-control" name="Keyword" placeholder="Search">
+                                    </div>
+                                </div>
                             </div>
 
                         </div> <!-- form-area /-->
@@ -129,16 +128,16 @@
         </div>
     </div>
 
-    	<!-- view Modal -->
-	<div class="modal fade" id="view_model">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h3 class="modal-title h5">View API Details</h3>     	
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				</div>
-				<div ng-include="templateURLEdit"></div>
-			</div>
-		</div>
-	</div>
-</div><!-- Body/ --> 
+    <!-- view Modal -->
+    <div class="modal fade" id="view_model">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title h5">View API Details</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div ng-include="templateURLEdit"></div>
+            </div>
+        </div>
+    </div>
+</div><!-- Body/ -->
